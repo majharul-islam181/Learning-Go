e54314f126c42819745005603ca5c48924ff3ae4
Md. Majharul Islam <86792533+majharul-islam181@users.noreply.github.com>
2025-03-25 10:07:03 +0600

    string package updated.

--- 21.Strings_package/main.go
+++ 21.Strings_package/main.go
@@ -5,24 +5,21 @@ import (
 	"strings"
 )
 
-func main(){
+func main() {
 	data := "apple, orange, banana"
-	parts := strings.Split(data,",")
-	fmt.Println("datat is : ", parts)
-
+	parts := strings.Split(data, ",")
+	fmt.Println("data is : ", parts) // [apple  orange  banana]
 
 	str := "one two three four two two five five"
 	cout := strings.Count(str, "two")
-	fmt.Println("Count is : ", cout)
+	fmt.Println("Count is : ", cout) // 3
 
-
-	str = "    Heloo World!                           ";
+	str = "    Heloo World!                           "
 	trim := strings.TrimSpace(str)
-	fmt.Println("Trim space : ", trim)
-
+	fmt.Println("Trim space : ", trim) // Heloo World!
 
 	str1 := "Prince"
 	str2 := "Hudai"
-	result := strings.Join([]string{str1, str2}, ",")
-	fmt.Println("result is : ",result)
+	result := strings.Join([]string{str1, "majharul", str2}, ",")
+	fmt.Println("result is : ", result) // Prince,Hudai
 }
